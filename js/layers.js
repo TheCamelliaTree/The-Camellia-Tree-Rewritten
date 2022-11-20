33e4208c34a3c04afc9c73a037ff24bd3b08a3df
TheCamelliaTree <anthonydc0630@gmail.com>
2022-11-19 22:44:54 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -70,5 +70,31 @@ addLayer("s", {
         {key: "s", description: "S: Create a Song", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
+}),
+addLayer("a", {
+    name: "albums", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(250),
+    }},
+    color: "#FFFFFF",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "albums", // Name of prestige currency
+    baseResource: "songs", // Name of resource prestige is based on
+    baseAmount() {return player.s.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.0001, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        return new Decimal(1)
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "a", description: "A: Create an Album", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasUpgrade('s', 22)}
 })
-
