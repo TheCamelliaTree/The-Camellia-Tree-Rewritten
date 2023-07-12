babaad9951fd2b1ebacce0d49fbb01d52dd90337
TheCamelliaTree <92651830+TheCamelliaTree@users.noreply.github.com>
2023-07-12 01:24:24 -0400

    yes

--- js/mod.js
+++ js/mod.js
@@ -1,11 +1,11 @@
 let modInfo = {
-	name: "The Camellia Tree",
-	id: "Camellia",
-	author: "Not Cametek (Halloweeb#4371 collabing with Flustix#5433 (foreshadowing something))",
-	pointsName: "notes",
+	name: "Antreematter NG+3C",
+	id: "ang3cc",
+	author: "ArcanaEden (rotating_ilot)",
+	pointsName: "antimatter",
 	modFiles: ["layers.js", "tree.js"],
 
-	discordName: "The Camellia Tree",
+	discordName: "ArcanaEden's Trees",
 	discordLink: "https://discord.gg/BEa67qjXa4",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
 	offlineLimit: 2400,  // In hours
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.3.1",
-	name: "Potential Block in Japan",
+	num: "0.1",
+	name: "NG+3C? WHAT DOES IT MEAN!?",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
@@ -27,30 +27,11 @@ d = bug/grammatical fixes.<br>
 Credit to pg132's changelog format.<br>
 <br>
 <br>
-	<h3>v0.3.1</h3><br>
-		- Added a layer.<br>
-		- Added challenges to the new layer (omg camellia song reference if I didn't express that already (thank you GrievousKnight#8249 for the nerf)).<br>
-		- Added songs to the new layer (thx to Icecreamdude's "Incremental God Tree" code, do play the tree, it's good).<br>
-		- Added a song softcap due to inflation reasons.<br>
-		- Paroxysm is now finished.<br>
-		- Added some new layer upgrades.<br>
-		- Added the changelog format.<br>
-		- Fixed some bugs and grammar (thanks to some of you guys for finding them).<br>
-		- Endgame is unlocking Sound Voltex Tab.<br>
-	<h3>v0.2</h3><br>
-		- Added a new layer. (Members)<br>
-		- Added 2 Song Upgrades and 4 Paroxysm Upgrades and Milestones.<br>
-		- Fixed a softlocking problem.<br>
-		- Added 2 Paroxysm Buyables.<br>
-		- Added Endgame under Points.<br>
-		- Endgame is 1 Member.<br>
 	<h3>v0.1</h3><br>
-		- Added Songs, Albums, and Paroxysm Layer (not implemented).<br>
-		- Endgame is Paroxysm Layer.`
+		- What do you expect from the first release of this tree? ToL? <br>
+		- Added Antimatter and some Dimensions.`
 
-let winText = `Your songs went viral, too viral that you completed the tree! (Camellia if you see this I spent too long making this, so please tell me if this is good...)<br>
-			Please make sure to join Camellia's Official Discord Server if you support his music and possibly the tree as well!<br>
-			https://discord.gg/camellia`
+let winText = `Your Antimatter became so powerful that you went poof.`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
@@ -71,21 +52,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(0)
-	if (hasMilestone('p', 0)) gain = new Decimal(1)
-	if (hasUpgrade('s', 11)) gain = new Decimal(1)
-	if (hasUpgrade('s', 12)) gain = gain.times(2)
-	if (hasUpgrade('s', 13)) gain = gain.times(upgradeEffect('s', 13))
-	if (hasUpgrade('s', 15)) gain = gain.times(upgradeEffect('s', 15))
-	if (hasUpgrade('p', 12) && !inChallenge('b', 12)) gain = gain.pow(1.05)
-	if (hasUpgrade('s', 22)) gain = gain.times(upgradeEffect('s', 22))
-	if (hasUpgrade('m', 12)) gain = gain.pow(1.2)
-	if (hasUpgrade('m', 13)) gain = gain.pow(upgradeEffect('m', 13))
-	if (hasUpgrade('s', 24)) gain = gain.times(1672) //Superluminal
-	if (hasUpgrade('b', 11)) gain = gain.pow(1.234)
-	if (hasUpgrade('b', 12)) gain = gain.pow(upgradeEffect('b', 12)) //reference to Bangin' Burst!'s bpm of 234
-	gain = gain.times(tmp.a.effect)
-	gain = gain.times(buyableEffect('p', 12))
-	if (inChallenge('b', 11)) gain = gain.pow(0.69) //nice
+gain = buyableEffect('a', 11)
 	return gain
 }
 
@@ -94,11 +61,11 @@ function addedPlayerData() { return {
 }}
 
 // Display extra things at the top of the page
-var displayThings = [() => `Current Endgame: Sound Voltex Tab unlocked.<br> ${inChallenge('b', 12) ? ` T1M3 UN!17 KU23HA C0N5UM35 A77: ${format(player.b.ktime)}s` : ''}<br><h1> ${inChallenge('b', 12) && player.b.time>88.8 && player.b.time<100.5 ? ` KU23HA W177 C0N5UM3 A77.` : ''}${inChallenge('b', 12) && player.b.time>100.5 && player.b.time<111.75 ? ` 1! 15 100 7A13.` : ''}<h1>${inChallenge('b', 12) && player.b.time>111.75 ? ` N0W P32!5H!!!` : ''}`]
+var displayThings = [() => `Current Endgame: 1e33/1 Dc Antimatter.`]
 
 // Determines when the game "ends"
 function isEndgame() {
-	return hasMilestone('b', 9)
+	return player.points.gte(1e33)
 }
 
 
