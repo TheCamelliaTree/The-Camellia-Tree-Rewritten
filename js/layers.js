474759cb3eb6f9c69061dfe5b5b8c3dba2e46ef6
TheCamelliaTree <92651830+TheCamelliaTree@users.noreply.github.com>
2024-01-09 23:02:18 -0500

    II

--- js/layers.js
+++ js/layers.js
@@ -1,11 +1,44 @@
-<div v-if="tmp.world.effect.timeLimit" id="timer" class="fullWidth">
-<h1><span>{{
-    Math.floor(tmp.world.effect.timeLeft / 60).toLocaleString("en-US")
-}}</span>:<span>{{
-    Math.floor((tmp.world.effect.timeLeft / 10) % 6).toLocaleString("en-US")
-}}</span><span>{{
-    Math.floor(tmp.world.effect.timeLeft % 10).toLocaleString("en-US")
-}}</span>.<span style="font-size:.5em">{{
-    Math.floor((tmp.world.effect.timeLeft * 10) % 10).toLocaleString("en-US")
-}}</span></h1>
-</div>
+addLayer("p", {
+    startData() { return {                  // startData is a function that returns default data for a layer. 
+        unlocked: true,                     // You can add more variables here to add them to your layer.
+        points: new Decimal(0),
+        ii: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
+    }},
+
+    color: "#4BDC13",                       // The color for this layer, which affects many elements.
+    resource: "prestige points",            // The name of this layer's main prestige resource.
+    row: 0,                                 // The row this layer is on (0 is the first row).
+
+    baseResource: "points",                 // The name of the resource your prestige gain is based on.
+    baseAmount() { return player.points },  // A function to return the current amount of baseResource.
+
+    requires: new Decimal(10),              // The amount of the base needed to  gain 1 of the prestige currency.
+                                            // Also the amount required to unlock the layer.
+
+    type: "normal",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
+
+    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
+        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
+    },
+    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
+        return new Decimal(1)
+    },
+
+    layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
+    effect() {
+        let eff = {};
+
+        let chal = layers.p.challenges[player.p.activeChallenge];
+        if (chal?.timeLimit) {
+            eff.timeLimit = true;
+            eff.timeLeft = chal.timeLimit - player.p.ii;
+        }
+        else {
+            eff.timeLeft = tmp.p.effect?.timeLeft ?? 0;
+        }},
+
+    challenges: {
+        
+    },
+})
