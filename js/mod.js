86abe3848cf1906202346d7c701ed75c7c4b67d1
TheCamelliaTree <92651830+TheCamelliaTree@users.noreply.github.com>
2024-01-23 22:48:52 -0500

    181

--- js/mod.js
+++ js/mod.js
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.7",
-	name: "NEW LONGEST STAT!?",
+	num: "0.8",
+	name: "did I add too many stats this time",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
@@ -44,7 +44,10 @@ let changelog = `<h1>Changelog:</h1><br>
 		- fixed a little error<br>
 	<h3>v0.7: NEW LONGEST STAT!?</h3><br>
 		- added 20 stats, the last one being so long that it's not gonna fit that screen<br>
-		- Endgame: 100th Stat (FINAL UPDATE FOR THIS MONTH)`
+		- Endgame: 100th Stat (FINAL UPDATE FOR THIS MONTH)<br>
+	<h3>v0.8: did I add too many stats this time</h3><br>
+		- added 81 stats, wait what-<br>
+		- Endgame: 181st stat, I expect this within 5 days`
 
 let winText = `how the fuck did you complete this tree, it was suppose to take 1000 hours not 1000 seconds`
 
@@ -176,6 +179,87 @@ function addedPlayerData() { return {
 		[new Decimal(0),"USAO",false,new Decimal(0)],//98
 		[new Decimal(0),"Kobaryo",false,new Decimal(0)],//99
 		[new Decimal(0),"The Amount of Tentacular Aliens' Extraterrestrial Jungle Dance Music In a Super-Ultra-Gigangtic-Mega U.F.O (it may be U.U.F.O.) flying over Illinois st. Destroying The Forest With A Would Be Machinegun Psystyle (And more genre switches) And A Blue-Eyed Skeleton With A Blue Jacket (It May Be Sans) Cycling through Baseballs And Base-Drops Entering The Jungle And Getting Fried From Furry Cannon From A Muzzle Facing (Long muzzled version) from Wacca Causing The May-be U.U.F.O Facing Wolves Thinking They Are Standing Towards Enemies, (From Lanota's not so hidden boss), And WYSI (When You See It) A New Challenger Approaches. Getting More Than 727 PP (M1LL10N PP) Compute It With Some Devilish Alchaolic Gears (or something im not sure) Getting GHOST-ed, (With GHOUL-ish Mixes) After The SLIME INCIDENT Trying To Purge My Existence From This World While Looking For Edge On Ground at AREA 52 Injecting The CICADA 3302 Villain Virus Liquating The Would Be Flamewall To Give Newspapers For Magicians In NIGHTMARE CITY While Having A Paroxysm With The Would Be U.U.F.O. Screaming The Immortal Scream In The Red Room While Spinning Eternally Exiting This Earths Atomosphere and Planet Shaping The Would Be Earths Atomosphere With A Nacreous Snowmelt and Lighting it up With a Crystallized KillerToy Flying Wit Me having Terabyte Connection That is Asking Why Do You Hate Me While Under Construxion With ΩΩPARTS While Inputting a Bad Access (From a Moe Maid) Into Their Spine While Creating a Nasty Nasty Spell That Blows Up a Nuclear Star Creating a Reality Distortion While Friending Nanahira On Bassbook Lol While Raising The BPM to 2021 While In The Bermuda Triangle While Creating another Nuclear Star Called #1f1e33 (#00102g Long Colored Version) While Trying To Break The Silence And Dance With Silence While Dealing With Completeness Under Incompleteness In A Blastix Riotz Asking R U Still xxxx? While Feeling Seasickness Going To The First Town Of This Journey While Dancing On The Mars And Trying To Find Proof Of A Hero (from a Monster Hunter) While Slicing Toxic Violet Cubes In Beat Saber While Volt Tackling a Galaxy Collision That Is A Dyscontrolled Galaxy Burst That Is Close To A Galaxy Collapse Leaving Scattered Faith Crosse",false,new Decimal(0)],//100
+		[new Decimal(0),"Cyaegha",true,new Decimal(0)], //101
+        [new Decimal(0),"Cosmica",false,new Decimal(0)],//102
+		[new Decimal(0),"Singularitie",false,new Decimal(0)],//103
+		[new Decimal(0),"Memory Forest",false,new Decimal(0)],//104
+		[new Decimal(0),"Stronghold",false,new Decimal(0)],//105
+		[new Decimal(0),"Silent Rushe",false,new Decimal(0)],//106
+		[new Decimal(0),"Moonheart",false,new Decimal(0)],//107
+		[new Decimal(0),"Romance War",false,new Decimal(0)],//108
+		[new Decimal(0),"Blossom",false,new Decimal(0)],//109
+		[new Decimal(0),"Party Vinyl",false,new Decimal(0)],//110
+		[new Decimal(0),"Flashback",false,new Decimal(0)],//111
+		[new Decimal(0),"Callima Karma",false,new Decimal(0)],//112
+		[new Decimal(0),"Lost",false,new Decimal(0)],//113
+		[new Decimal(0),"Loveless Dresse",false,new Decimal(0)],//114
+		[new Decimal(0),"Defection",false,new Decimal(0)],//115
+		[new Decimal(0),"Arcahv",false,new Decimal(0)],//116
+		[new Decimal(0),"Antagonism",false,new Decimal(0)],//117
+		[new Decimal(0),"Equilibrium",false,new Decimal(0)],//118
+		[new Decimal(0),"BLRINK",false,new Decimal(0)],//119
+		[new Decimal(0),"Vindication",false,new Decimal(0)],//120
+		[new Decimal(0),"Particle Art",false,new Decimal(0)],//121
+		[new Decimal(0),"The Message",false,new Decimal(0)],//122
+		[new Decimal(0),"Halcyon",false,new Decimal(0)],//123
+		[new Decimal(0),"Ether Strike",false,new Decimal(0)],//124
+		[new Decimal(0),"conflict",false,new Decimal(0)],//125
+		[new Decimal(0),"SOUNDWiTCH",false,new Decimal(0)],//126
+		[new Decimal(0),"Iconoclast",false,new Decimal(0)],//127
+		[new Decimal(0),"Solitary Dream",false,new Decimal(0)],//128
+		[new Decimal(0),"Sheriruth",false,new Decimal(0)],//129
+		[new Decimal(0),"Lumia",false,new Decimal(0)],//130
+		[new Decimal(0),"G e n g a o z o ",false,new Decimal(0)],//131
+		[new Decimal(0),"False Embellishment",false,new Decimal(0)],//132
+		[new Decimal(0),"Turbocharger",false,new Decimal(0)],//133
+		[new Decimal(0),"blue comet",false,new Decimal(0)],//134
+		[new Decimal(0),"HIVEMIND",false,new Decimal(0)],//135
+		[new Decimal(0),"Vandalism",false,new Decimal(0)],//136
+		[new Decimal(0),"Glow",false,new Decimal(0)],//137
+		[new Decimal(0),"Purple Verse",false,new Decimal(0)],//138
+		[new Decimal(0),"Bamboo",false,new Decimal(0)],//139
+		[new Decimal(0),"Faint Light",false,new Decimal(0)],//140
+		[new Decimal(0),"GIMMICK",false,new Decimal(0)],//141
+		[new Decimal(0),"Lawless Point",false,new Decimal(0)],//142
+		[new Decimal(0),"To The Furthest Dream",false,new Decimal(0)],//143
+		[new Decimal(0),"Manic Jeer",false,new Decimal(0)],//144
+		[new Decimal(0),"PRIMITIVE LIGHT",false,new Decimal(0)],//145
+		[new Decimal(0),"Crimson Throne",false,new Decimal(0)],//146
+		[new Decimal(0),"Capella",false,new Decimal(0)],//147
+		[new Decimal(0),"PUPA",false,new Decimal(0)],//148
+		[new Decimal(0),"INTERNET OVERDOSE",false,new Decimal(0)],//149
+		[new Decimal(0),"BUCHiGiRE Berserker",false,new Decimal(0)],//150
+		[new Decimal(0),"Scarlet Cage",false,new Decimal(0)],//151
+		[new Decimal(0),"Altale",false,new Decimal(0)],//152
+		[new Decimal(0),"Dreadnought",false,new Decimal(0)],//153
+		[new Decimal(0),"Phantasia",false,new Decimal(0)],//154
+		[new Decimal(0),"Filament",false,new Decimal(0)],//155
+		[new Decimal(0),"Einherjar Joker",false,new Decimal(0)],//156
+		[new Decimal(0),"IZANA",false,new Decimal(0)],//157
+		[new Decimal(0),"Shades of Light in the Transcendent Realm",false,new Decimal(0)],//158
+		[new Decimal(0),"Grimheart",false,new Decimal(0)],//159
+		[new Decimal(0),"Lost Desire",false,new Decimal(0)],//160		
+		[new Decimal(0),"Arghena",false,new Decimal(0)],//161
+		[new Decimal(0),"Bookmaker",false,new Decimal(0)],//162
+		[new Decimal(0),"REKKA RESONANCE",false,new Decimal(0)],//163
+		[new Decimal(0),"SAIKYO STRONGER",false,new Decimal(0)],//164
+		[new Decimal(0),"dropdeath",false,new Decimal(0)],//165
+		[new Decimal(0),"Malicious Mischance",false,new Decimal(0)],//166
+		[new Decimal(0),"Aegleseeker",false,new Decimal(0)],//167
+		[new Decimal(0),"Galaxy Friend",false,new Decimal(0)],//168
+		[new Decimal(0),"Tempestissimo",false,new Decimal(0)],//169
+		[new Decimal(0),"Axium Crise",false,new Decimal(0)],//170
+		[new Decimal(0),"Fracture Ray",false,new Decimal(0)],//171
+		[new Decimal(0),"Grievous Ladie",false,new Decimal(0)],//172
+		[new Decimal(0),"PRAGMATISM -RESURRECTION-",false,new Decimal(0)],//173
+		[new Decimal(0),"Dantalion",false,new Decimal(0)],//174
+		[new Decimal(0),"AttraqtiA",false,new Decimal(0)],//175
+		[new Decimal(0),"Pentiment",false,new Decimal(0)],//176
+		[new Decimal(0),"Infinite Strife,",false,new Decimal(0)],//177
+		[new Decimal(0),"World Ender",false,new Decimal(0)],//178
+		[new Decimal(0),"Testify",false,new Decimal(0)],//179
+		[new Decimal(0),"Arcana Eden",false,new Decimal(0)],//180		
+		[new Decimal(0),"#1f1e33",false,new Decimal(0)],//181
     ],
 }}
 
